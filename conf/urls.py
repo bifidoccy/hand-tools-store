ca5bcac334a34063f7261bf8645ef307c9a2b03f
mymotherati <100pofarengeitu@gmail.com>
2023-04-27 20:18:27 +0300

    conf: modified

--- conf/urls.py
+++ conf/urls.py
@@ -1,10 +1,16 @@
 """
 Main URLConf for hand-tools-store
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('ckeditor/', include('ckeditor_uploader.urls')),
     path('', include('shop.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
